3b8c12d6605dacec2a4013e1ccc467307c7b6885
yuan <115965720+sunyuanting@users.noreply.github.com>
2025-05-02 12:28:55 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ let graphicsLayer;
 
 function setup() {
   createCanvas(windowWidth, windowHeight); // 全螢幕畫布
-  capture = createCapture(VIDEO); // 從攝影機擷取影像
+  capture = createCapture(VIDEO, { flipped: true }); // 從攝影機擷取影像
   capture.size(windowWidth * 0.8, windowHeight * 0.8); // 設定影像大小為視窗的 80%
   capture.hide(); // 隱藏預設的影像元素
   graphicsLayer = createGraphics(capture.width, capture.height); // 建立一個與影像大小相同的圖層
