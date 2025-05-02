69d1a0672304b0cf22cb318de94c721982aa44db
yuan <115965720+sunyuanting@users.noreply.github.com>
2025-05-02 12:13:34 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,12 +12,11 @@ function setup() {
 function draw() {
   background('#ecf8f8'); // 設定背景顏色為 ecf8f8
 
-  // 繪製攝影機影像，直接水平翻轉
-  push(); // 儲存當前繪圖狀態
-  translate(width, 0); // 將原點移動到右上角
-  scale(-1, 1); // 水平翻轉影像
-  image(capture, 0, (height - capture.height) / 2, capture.width, capture.height); // 繪製翻轉後的影像
-  pop(); // 恢復原始繪圖狀態
+  push();
+  translate((width + capture.width) / 2, (height - capture.height) / 2); // 移動到正確位置
+  scale(-1, 1); // 左右翻轉
+  image(capture, 0, 0); // 繪製翻轉後的影像
+  pop();
 
   // 在影像上方繪製圖層
   graphicsLayer.clear(); // 清除圖層內容
